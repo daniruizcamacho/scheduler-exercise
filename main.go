2c31656f5c6f2a41a1f7dd039e0316104199920a
daniruizcamacho <daniel.ruizcamacho@gmail.com>
2024-07-13 18:44:51 +0200

    Version 2

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"os/signal"
+	"sync"
 	"time"
 )
 
@@ -24,62 +25,62 @@ func main() {
 // - 'concurrency' tasks executing at a time.
 // - At least 500msec between execution for the same task.
 type Scheduler struct {
-	tasks           []int
-	concurrency     int
+	tasks           map[int]Task
 	refreshFn       func() []int
 	refreshDuration time.Duration
-	removeProcessed chan int
-	processedTasks  map[int]bool
+	runningTasks    chan int
+}
+
+type Task struct {
+	ID            int
+	lastExecution time.Time
 }
 
 func NewScheduler(concurrency int, refreshFn func() []int, refreshDuration time.Duration) *Scheduler {
 	return &Scheduler{
-		concurrency:     concurrency,
+		tasks:           make(map[int]Task),
 		refreshFn:       refreshFn,
 		refreshDuration: refreshDuration,
-		tasks:           []int{},
-		removeProcessed: make(chan int),
-		processedTasks:  make(map[int]bool),
+		runningTasks:    make(chan int, concurrency),
 	}
 }
 
 func (s *Scheduler) Loop(ctx context.Context) {
 	ticker := time.NewTicker(s.refreshDuration)
+	wg := sync.WaitGroup{}
+	sync := sync.Mutex{}
 	defer ticker.Stop()
 	for {
 		select {
 		case <-ticker.C:
-			s.tasks = append(s.tasks, s.refreshFn()...)
+			go func() {
+				for _, v := range s.refreshFn() {
+					fmt.Println("new task", v)
+					sync.Lock()
+					s.tasks[v] = Task{ID: v}
+					sync.Unlock()
+					s.runningTasks <- v
+					fmt.Println("task", v, "added to running tasks")
+				}
+			}()
 		case <-ctx.Done():
+			wg.Wait()
 			return
-		case task := <-s.removeProcessed:
-			fmt.Println("removed", task)
-			fmt.Println(s.processedTasks)
-			s.processedTasks[task] = false
-		default:
-			fmt.Println(s.tasks)
-			time.Sleep(1 * time.Second)
-			if len(s.tasks) == 0 {
+		case runningTask := <-s.runningTasks:
+			if !s.tasks[runningTask].lastExecution.IsZero() && s.tasks[runningTask].lastExecution.Add(500*time.Millisecond).After(time.Now()) {
+				fmt.Println("task", runningTask, "not ready to execute")
 				continue
 			}
-
-			numberOfTasksToExecute := s.concurrency
-			if len(s.tasks) < s.concurrency {
-				numberOfTasksToExecute = len(s.tasks)
-			}
-
-			tasksToExecute := s.tasks[:numberOfTasksToExecute]
-			s.tasks = s.tasks[numberOfTasksToExecute:]
-			for _, task := range tasksToExecute {
-				fmt.Println("processed", task)
-				fmt.Println(s.processedTasks)
-				if s.processedTasks[task] {
-					continue
-				}
-				s.processedTasks[task] = true
-				go execute(task)
-				go s.remove(task)
-			}
+			task := s.tasks[runningTask]
+			task.lastExecution = time.Now()
+			sync.Lock()
+			s.tasks[runningTask] = task
+			sync.Unlock()
+			go func(task int) {
+				wg.Add(1)
+				defer wg.Done()
+				execute(task)
+			}(runningTask)
 		}
 	}
 }
@@ -100,9 +101,4 @@ func tasks() []int {
 
 func execute(task int) {
 	fmt.Println("executed", task)
-}
-
-func (s *Scheduler) remove(task int) {
-	time.Sleep(500 * time.Millisecond)
-	s.removeProcessed <- task
 }
